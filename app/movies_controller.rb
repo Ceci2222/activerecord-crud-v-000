119761631c24aff07fa17fa06b20298a470e297c
Marie Burns <mcburns2222@gmail.com>
2019-02-16 02:23:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
   
   Movie.create do |m|
-    movie.title = "Home Alone"
-    movie.release_date = 1990
+    @title = "Home Alone"
+    @release_date = 1990
   end
 end
 
