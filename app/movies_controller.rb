51003b1a12bfdcf5e4a98452d5069d725153a9b5
Marie Burns <mcburns2222@gmail.com>
2019-02-16 02:36:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {})
   # release_date == 1990
   
   Movie.create do |m|
-    @title = args[title:]
-    movie.release_date = 1990
+    title = args[:title]
+    release_date = 1990
   end
 end
 
